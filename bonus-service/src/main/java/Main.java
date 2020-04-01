515f2098272df650f3b9637b10be462c5ccd3c59
Ekaterina Rymareva <rymareva_ea@mail.ru>
2020-04-02 01:11:43 +0500

    Fix error, fixes #1

--- bonus-service/src/main/java/Main.java
+++ bonus-service/src/main/java/Main.java
@@ -4,8 +4,9 @@ public class Main {
 
     long amount = 1000_60;
     boolean registered = true;
+    long actual = service.calculate(amount, registered);
 
-    service.calculate(amount, registered);
+    System.out.println(actual);
   }
 }
 
